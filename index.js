2cfb59f27a6775a1d683cb4f434ad766448cf1d9
umedjon01 <najottalimfn15@gmail.com>
2024-01-17 01:06:22 +0500

    HOMEWORK

---
+++ index.js
@@ -0,0 +1,173 @@
+// 1-chi masala
+
+// let num1 = 3;
+// let num2 = 12;
+// let num3 = 1;
+
+// let kupaytma = 1;
+
+// if (num1 % 3 == 0) {
+//   kupaytma *= num1;
+// }
+// if (num2 % 3 == 0) {
+//   kupaytma *= num2;
+// }
+// if (num3 % 3 == 0) {
+//   kupaytma *= num3;
+// }
+// console.log(kupaytma);
+
+// 2-chi masala
+
+// let num = 135;
+
+// let birlar = num % 10;
+
+// let on = num % 100;
+
+// let yuz = num % 1000;
+
+// let onlar = (on - birlar) / 10;
+
+// let yuzlar = (yuz - on) / 100;
+
+// let result = yuzlar + onlar + birlar;
+
+// console.log(result);
+
+// 3-chi masala
+
+// let num = 333;
+// let kupaytma = 1;
+
+// let birlar = num % 10;
+
+// let on = num % 100;
+
+// let yuz = num % 1000;
+
+// let onlar = (on - birlar) / 10;
+
+// let yuzlar = (yuz - on) / 100;
+
+// if (birlar % 2 != 0) {
+//   kupaytma *= birlar;
+// }
+// if (onlar % 2 != 0) {
+//   kupaytma *= onlar;
+// }
+// if (yuzlar % 2 != 0) {
+//   kupaytma *= yuzlar;
+// }
+// console.log(kupaytma);
+
+// 4-chi masala
+
+// let num1 = 35;
+// let num2 = 25;
+// let num3 = 70;
+
+// if (num1 % 5 == 0) {
+//   if (num1 % 7 == 0) {
+//     console.log("Birinchi son - 5 va 7 ga karrali");
+//   }
+// }
+
+// if (num2 % 5 == 0) {
+//   if (num2 % 7 == 0) {
+//     console.log("Ikkinchi son  - 5 va 7 ga karrali");
+//   }
+// }
+// if (num3 % 5 == 0) {
+//   if (num3 % 7 == 0) {
+//     console.log(" Uchinchi son - 5 va 7 ga karrali");
+//   }
+// }
+
+// 5chi - masala
+
+// let harf = "";
+
+// let a = "a";
+// let e = "e";
+// let i = "i";
+// let o = "o";
+// let u = "u";
+// let oo = "o'";
+// if (harf === a) {
+//   console.log(harf + " unli harf ekan.");
+// } else if (harf === e) {
+//   console.log(harf + " unli harf ekan.");
+// } else if (harf === i) {
+//   console.log(harf + " unli harf ekan.");
+// } else if (harf === o) {
+//   console.log(harf + " unli harf ekan.");
+// } else if (harf === u) {
+//   console.log(harf + " unli harf ekan.");
+// } else if (harf === oo) {
+//   console.log(harf + " unli harf ekan.");
+// } else {
+//   console.log(harf + " undosh harf ekan.");
+// }
+
+// 6 - chi masa'la
+
+// let son1 = 9;
+// let son2 = 3;
+// let ope = "*";
+
+// if (ope == "+") {
+//   console.log(son1 + son2);
+// }
+// if (ope == "-") {
+//   console.log(son1 - son2);
+// }
+// if (ope == "*") {
+//   console.log(son1 * son2);
+// }
+// if (ope == "/") {
+//   console.log(son1 / son2);
+// }
+
+//  7-chi masala
+
+// let son1 = 4;
+// let son2 = 4;
+// let son3 = 5;
+
+// let sum = 0;
+// if (son1 + son2 > son3) {
+//   sum = sum +1;
+// }
+// if (son1 + son3 > son2) {
+//   sum = sum +1;
+// }
+// if (son2 + son3 > son1) {
+//   sum = sum +1;
+// }
+// if (sum == 3) {
+//   console.log("Uchburchak yaratish mumkin!");
+// } else {
+//   console.log("Uchburchak yaratish mumkin emas!");
+// }
+
+// 8 -chi masala
+
+// let num = +prompt("Son kiriting");
+
+// let birlar = num % 10;
+
+// let on = num % 100;
+
+// let yuz = num % 1000;
+
+// let onlar = (on - birlar) / 10;
+
+// let yuzlar = (yuz - on) / 100;
+
+// let res = birlar * 100 + onlar * 10 + yuzlar;
+// if (res == num) {
+//   console.log("Siz kiritgan son Palindrom");
+// } else {
+//   console.log("Siz kiritgan son Palindrom emas ");
+// }
